bcf1a79955af2635a2d09475377eff9e916c4652
Nishant Kohli <nishkohli96@gmail.com>
2021-09-07 05:04:54 +0530

    ios run successful

--- ios/ReactNativeLibsTests/ReactNativeLibsTests.m
+++ ios/ReactNativeLibsTests/ReactNativeLibsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RnTemplateTests : XCTestCase
+@interface ReactNativeLibsTests : XCTestCase
 
 @end
 
-@implementation RnTemplateTests
+@implementation ReactNativeLibsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
