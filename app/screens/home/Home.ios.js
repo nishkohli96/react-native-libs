8052656ac44fa5b66654d66c9685c97e074c39e5
Nishant Kohli <nishkohli96@gmail.com>
2021-09-07 05:50:39 +0530

    wip

--- app/screens/home/Home.ios.js
+++ app/screens/home/Home.ios.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { View, TouchableOpacity, Pressable, StyleSheet } from 'react-native';
 import { useTranslation } from 'react-i18next';
+import Clipboard from '@react-native-community/clipboard';
+
 import Header from '_Shared/Header';
 import {
     ThemedContainer,
@@ -10,6 +13,18 @@ import {
 
 const App = () => {
     const { t } = useTranslation('common');
+    const [pressText, setPressText] = React.useState('Press me');
+    const [copiedText, setCopiedText] = React.useState('');
+
+    const copyToClipboard = () => {
+        Clipboard.setString('this is a copied string');
+    };
+
+    const fetchCopiedText = async () => {
+        const text = await Clipboard.getString();
+        console.log('text: ', text);
+        setCopiedText(text);
+    };
 
     return (
         <ThemedContainer>
@@ -18,9 +33,59 @@ const App = () => {
                 <ThemedCard>
                     <ThemedText>Welcome to the HomeScreen !!!</ThemedText>
                 </ThemedCard>
+
+                <ThemedCard style={styles.copyCard}>
+                    <ThemedText style={styles.headerText}>
+                        Click The Appropriate Btn to see their functionality
+                    </ThemedText>
+
+                    <Pressable
+                        onPressIn={() => setPressText('normal press')}
+                        onPressOut={() => setPressText('Press Me')}
+                        onLongPress={() => setPressText('On Long Press...')}>
+                        <View style={styles.pressView}>
+                            <ThemedText style={styles.pressedText}>
+                                {pressText}
+                            </ThemedText>
+                        </View>
+                    </Pressable>
+
+                    <TouchableOpacity onPress={copyToClipboard}>
+                        <View style={styles.pressView}>
+                            <ThemedText>
+                                Click here to copy to Clipboard
+                            </ThemedText>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={fetchCopiedText}>
+                        <ThemedText>View copied text</ThemedText>
+                    </TouchableOpacity>
+
+                    <ThemedText>
+                        {copiedText}
+                    </ThemedText>
+                </ThemedCard>
             </ThemedBody>
         </ThemedContainer>
     );
 };
+
+const styles = StyleSheet.create({
+    copyCard: { marginTop: 30 },
+    pressView: {
+        borderColor: 'grey',
+        padding: 10,
+        backgroundColor: 'silver',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    pressedText: {
+        color: 'red',
+    },
+    copiedText: {
+        marginTop: 10,
+        color: '#007aba',
+    },
+});
 
 export default App;
