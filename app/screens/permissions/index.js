45a04978ad2a45dc7ab9829e2b88c144f4ca3246
Nishant Kohli <nishkohli96@gmail.com>
2021-09-19 03:11:29 +0530

    location wip

--- app/screens/permissions/index.js
+++ app/screens/permissions/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import IoniconsI from 'react-native-vector-icons/Ionicons';
 
 import {
     ThemedContainer,
@@ -10,9 +11,12 @@ import {
     ThemedText,
 } from '_Shared/Comps.themed';
 import Header from '_Shared/Header';
+import { useThemeStore } from '_Store/theme.store';
+import CommonStyles from '_Themes/CommonStyles';
 
 const Permissions = () => {
     const navigation = useNavigation();
+    const { themeObj } = useThemeStore();
 
     return (
         <ThemedContainer>
@@ -25,7 +29,28 @@ const Permissions = () => {
                 <TouchableOpacity
                     onPress={() => navigation.navigate('ContactsList')}>
                     <ThemedCard style={styles.card}>
-                        <ThemedText>Contacts</ThemedText>
+                        <ThemedText>
+                            <IoniconsI
+                                name="person-outline"
+                                color={themeObj.colors.text}
+                                size={CommonStyles.icons.drawerIcon}
+                            />{' '}
+                            Contacts
+                        </ThemedText>
+                    </ThemedCard>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('ContactsList')}>
+                    <ThemedCard style={styles.card}>
+                        <ThemedText>
+                            <IoniconsI
+                                name="location-outline"
+                                color={themeObj.colors.text}
+                                size={CommonStyles.icons.drawerIcon}
+                            />{' '}
+                            Location
+                        </ThemedText>
                     </ThemedCard>
                 </TouchableOpacity>
             </ThemedBody>
